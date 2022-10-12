9f49e4c6fb94a280f17295c0fcb918cf23128d62
take-cheeze <takechi101010@gmail.com>
2022-10-12 23:39:07 +0900

    Support op with attributes

--- onnx2torchscript/onnx2torchscript.py
+++ onnx2torchscript/onnx2torchscript.py
@@ -1,14 +1,15 @@
 from ast import Call
+from curses.ascii import isalnum
 from fnmatch import fnmatch
-from typing import Callable, Dict, Optional, Tuple
-
+from pyclbr import Function
+from typing import Any, Callable, Dict, Optional, Tuple
 
 import onnx
 import torch
 
 
-_op_table: Dict[int, Dict[str, torch._C.Graph]] = {}
-_version_cache: Dict[Tuple[str, int], torch._C.Graph] = {}
+_op_table: Dict[int, Dict[str, torch._C.ScriptFunction]] = {}
+_version_cache: Dict[Tuple[str, int], torch._C.ScriptFunction] = {}
 
 
 def onnx_op(op_type: str, opset_version: int = 0, domain = "") -> Callable:
@@ -16,10 +17,8 @@ def onnx_op(op_type: str, opset_version: int = 0, domain = "") -> Callable:
         ret = torch.jit.script(f)
         _op_table.setdefault(opset_version, {})
         assert op_type not in _op_table[opset_version]
-        _op_table[opset_version][op_type] = ret.graph
+        _op_table[opset_version][op_type] = ret
         _version_cache = {}  # Clear cache
-        print(ret)
-        # print(ret.graph)
         return ret
     return fn
 
@@ -28,23 +27,23 @@ def onnx_op(op_type: str, opset_version: int = 0, domain = "") -> Callable:
 def op_Add(a: torch.Tensor, b: torch.Tensor) -> torch.Tensor:
     return a + b
 
-
 @onnx_op("Gemm", 11)
 def op_Gemm(
     a: torch.Tensor, b: torch.Tensor, c: Optional[torch.Tensor] = None,
-    alpha = 1.0, beta = 1.0, transA = 0, transB = 0
+    *,
+    alpha: float, beta: float, transA: int, transB: int
 ) -> torch.Tensor:
     if transA:
         a = a.swapaxes(-1, -2)
     if transB:
         b = b.swapaxes(-1, -2)
     if c is not None:
-        return torch.addbmm(c, a, b, beta=beta, alpha=alpha)
+        return torch.addmm(c, a, b, beta=beta, alpha=alpha)
     else:
         return torch.mm(a, b) * alpha
 
 
-def graph_of_onnx_op(op_type: str, opset_version: int = 0) -> torch._C.Graph:
+def get_onnx_ts(op_type: str, opset_version: int = 0, domain: str = "") -> torch._C.ScriptFunction:
     k = (op_type, opset_version)
     if k in _version_cache:
         return _version_cache[k]
@@ -55,11 +54,36 @@ def graph_of_onnx_op(op_type: str, opset_version: int = 0) -> torch._C.Graph:
             op_type not in _op_table[opset_version]:
         opset_version -= 1
         if opset_version == 0:
-            raise NotImplementedError(f"${op_type}-${opset_version} not found")
+            raise NotImplementedError(f"{domain}::{op_type}-{opset_version} not found")
         continue
 
     ret = _version_cache[k] = _op_table[opset_version][op_type]
     return ret
+
+
+def _copy_node(dst: torch._C.Node, src: torch._C.Node, t_values: Dict[str, str], values: Dict[str, torch._C.Value]) -> None:
+    dst.copyAttributes(src)
+    for b in src.blocks():
+        new_b = dst.addBlock()
+
+        for n in b.nodes():
+            new_inputs = []
+            for i in n.inputs():
+                new_i = values[t_values[i.debugName()]]
+                new_inputs.append(new_i)
+            new_n = new_b.addNode(n.kind(), new_inputs)
+            for o in n.outputs():
+                new_o = new_n.addOutput()
+                new_o_name = o.debugName()
+                if new_o_name.isnumeric():
+                    new_o_name = f"{n.kind().split('::')[-1]}_{new_o_name}"
+                new_o.setDebugName(new_o_name)
+                t_values[o.debugName()] = new_o.debugName()
+                values[new_o.debugName()] = new_o
+            _copy_node(new_n, n, t_values, values)
+
+        for o in b.outputs():
+            new_b.registerOutput(values[t_values[o.debugName()]])
 
 
 def onnx2ts(model: onnx.ModelProto, verbose: bool = False) -> torch._C.Graph:
@@ -81,18 +105,53 @@ def onnx2ts(model: onnx.ModelProto, verbose: bool = False) -> torch._C.Graph:
     model_output_names = [v.name for v in model.graph.output]
 
     for o_n in model.graph.node:
-        t_g = graph_of_onnx_op(o_n.op_type, domain2opset[o_n.domain])
+        t_s = get_onnx_ts(o_n.op_type, domain2opset[o_n.domain], o_n.domain)
+        t_sch = t_s.schema
+        t_g = t_s.graph
+        o_sch = onnx.defs.get_schema(o_n.op_type, domain2opset[o_n.domain], o_n.domain)
 
         t_values: Dict[str, str] = {}
         for o_i, t_i in zip(o_n.input, t_g.inputs()):
             t_i.setDebugName(f"{o_n.name}_{t_i.debugName()}")
             t_values[t_i.debugName()] = o_i
 
+        o_attrs: Dict[str, onnx.AttributeProto] = {}
+        for name, o_sch_a in o_sch.attributes.items():
+            if o_sch_a.default_value is not None:
+                o_attrs[name] = o_sch_a.default_value
+        for o_a in o_n.attribute:
+            o_attrs[o_a.name] = o_a
+
+        o_attr_vals: Dict[str, Any] = {}
+        for name, o_a in o_attrs.items():
+            if o_a.type == onnx.AttributeProto.AttributeType.UNDEFINED:
+                continue
+
+            v = None
+            if o_a.type == onnx.AttributeProto.AttributeType.INT:
+                v = o_a.i
+            elif o_a.type == onnx.AttributeProto.AttributeType.FLOAT:
+                v = o_a.f
+            assert v is not None
+            o_attr_vals[name] = v
+
+        for t_a_idx, (t_a, t_i) in enumerate(zip(t_sch.arguments, t_g.inputs())):
+            if t_a.kwarg_only:
+                t_v = ret.insertConstant(o_attr_vals[t_a.name])
+                t_v.setDebugName(f"{o_n.name}_{t_i.debugName()}")
+                t_values[t_i.debugName()] = t_v.debugName()
+                values[t_v.debugName()] = t_v
+            elif t_a_idx >= len(o_n.input):
+                t_v = ret.insertConstant(t_a.default_value)
+                t_v.setDebugName(f"{o_n.name}_{t_a.name}")
+                t_values[t_i.debugName()] = t_v.debugName()
+                values[t_v.debugName()] = t_v
+
         for t_n in t_g.nodes():
             ins = [values[t_values[t_i.debugName()]] for t_i in t_n.inputs()]
             op_name = t_n.kind()
             c = ret.create(op_name, ins, len(list(t_n.outputs())))
-            c.copyAttributes(t_n)
+            _copy_node(c, t_n, t_values, values)
             for c_o, t_o in zip(c.outputs(), t_n.outputs()):
                 c_o.setDebugName(f"{o_n.name}_{t_n.kind().split('::')[-1]}_{t_o.debugName()}")
                 values[c_o.debugName()] = c_o
--- tests/onnx2torchscript_tests/test_ops.py
+++ tests/onnx2torchscript_tests/test_ops.py
@@ -1,19 +1,35 @@
 from turtle import forward
+from typing import Callable
 import onnx2torchscript as o2t
 
 import torch
 import onnx
 import tempfile
 
-def test_add():
+def run_op_test(f: Callable, *args):
     class M(torch.nn.Module):
-        def forward(self, a, b):
-            return a + b
+        def forward(self, *args):
+            return f(*args)
 
-    a, b = torch.randn(10), torch.randn(10)
-    f = tempfile.NamedTemporaryFile()
-    torch.onnx.export(M(), (a, b), f.name)
-    m: onnx.ModelProto = onnx.load_model(f.name)
+    tmp = tempfile.NamedTemporaryFile()
+    torch.onnx.export(M(), args, tmp.name)
+    m: onnx.ModelProto = onnx.load_model(tmp.name)
     ts = o2t.onnx2ts(m)
-    f = torch._C._create_function_from_graph(m.graph.name, ts)
-    assert torch.allclose(M()(a, b), f(a, b))
+    torch._C._jit_pass_lint(ts)
+    ts_f = torch._C._create_function_from_graph(m.graph.name, ts)
+    assert torch.allclose(M()(*args), ts_f(*args))
+
+def test_add():
+    run_op_test(lambda a, b: a + b, torch.randn(10), torch.randn(10))
+
+def test_gemm():
+    # o2t.onnx2torchscript.get_onnx_op("Gemm", 11)(
+    #     torch.randn(10, 10), torch.randn(10, 10),
+    #     alpha=1.0, beta=1.0, transA=0, transB=0)
+    run_op_test(
+        lambda a, b: torch.mm(a, b),
+        torch.randn(10, 10), torch.randn(10, 10))
+    run_op_test(
+        lambda c, a, b: torch.addmm(c, a, b),
+        torch.randn(10, 10), torch.randn(10, 10),
+        torch.randn(10, 10))
